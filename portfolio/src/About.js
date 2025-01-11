46b67390ac05bd2d4b0c2847d6c62a7f9465ec45
Nilanjan Roy <roy.nil@northeastern.edu>
2025-01-10 21:18:39 -0500

    Commit 3

---
+++ portfolio/src/About.js
@@ -0,0 +1,8 @@
+const About = () => (
+    <section id="about">
+      <h1>About Me</h1>
+      <p>Hello, I'm [Your Name]. A passionate developer...</p>
+    </section>
+  );
+  export default About;
+  
---
+++ portfolio/src/Header.js
@@ -0,0 +1,12 @@
+const Header = () => (
+    <header>
+      <nav>
+        <a href="#about">About</a>
+        <a href="#skills">Skills</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+  export default Header;
+  
