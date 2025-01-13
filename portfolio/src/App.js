4832eda2c036df999a90784ad5c58e71a9e608d6
Nilanjan Roy <roy.nil@northeastern.edu>
2025-01-13 13:43:35 -0500

    Commit 7

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
+import logo from './assets/logo1.png'; // Import your logo
 
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className="App-header">
+        {/* Display the logo */}
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to My Portfolio</h1>
       </header>
       <main>
-        <section>
+        <section id="about">
           <h2>About Me</h2>
           <p>
             Hi, I'm Nilanjan Roy. I am a web developer with a passion for
             creating beautiful and functional web applications.
           </p>
         </section>
-        <section>
+        <section id="projects">
           <h2>Projects</h2>
           <ul>
             <li>Project 1: [Some Java Project]</li>
@@ -23,7 +26,7 @@ function App() {
         </section>
       </main>
       <footer>
-        <p>© 2025 Your Name. All rights reserved.</p>
+        <p>© 2025 Nilanjan Roy. All rights reserved.</p>
       </footer>
     </div>
   );
