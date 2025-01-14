9b2cc573672359efc333f21d5dfce70d93f26cc8
Nilanjan Roy <roy.nil@northeastern.edu>
2025-01-14 17:44:13 -0500

    Commit 9

---
+++ portfolio/src/pages/Project2.js
@@ -0,0 +1,11 @@
+const Project2 = () => {
+    return (
+      <div>
+        <h1>Project 2: React Project</h1>
+        <p>Details about the React Project...</p>
+      </div>
+    );
+  };
+  
+  export default Project2;
+  
