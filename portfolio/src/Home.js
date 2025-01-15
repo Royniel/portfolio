d9edc00bd6fe6d8b89a416d513558b12a54c3996
Nilanjan Roy <roy.nil@northeastern.edu>
2025-01-15 11:19:00 -0500

    commit 11

--- portfolio/src/Home.js
+++ portfolio/src/Home.js
@@ -1,37 +1,57 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './App.css';
 import project1Image from './assets/project1.png';
 import project2Image from './assets/project2.png';
+import './App.css';
 
 const Home = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
   const navigate = useNavigate();
+
+  const slides = [
+    { image: project1Image, title: 'Project 1: Java Project', link: '/project1' },
+    { image: project2Image, title: 'Project 2: React Project', link: '/project2' },
+  ];
+
+  // Automatically move to the next slide every 3 seconds
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [slides.length]);
+
+  // Navigate to the previous slide
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
+  };
+
+  // Navigate to the next slide
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+  };
 
   return (
     <div className="Home">
-      <header>
-        <h1>Welcome to My Portfolio</h1>
-      </header>
-      <Carousel fade interval={3000}>
-        {/* Project 1 Slide */}
-        <Carousel.Item onClick={() => navigate('/project1')} style={{ cursor: 'pointer' }}>
-          <img className="d-block w-100" src={project1Image} alt="Project 1" />
-          <Carousel.Caption>
-            <h3>Project 1: Java Project</h3>
-            <p>Click to see more about this Java project.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        {/* Project 2 Slide */}
-        <Carousel.Item onClick={() => navigate('/project2')} style={{ cursor: 'pointer' }}>
-          <img className="d-block w-100" src={project2Image} alt="Project 2" />
-          <Carousel.Caption>
-            <h3>Project 2: React Project</h3>
-            <p>Click to see more about this React project.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <div className="carousel-container">
+        <div className="carousel-slide">
+          <img
+            src={slides[currentIndex].image}
+            alt={slides[currentIndex].title}
+            onClick={() => navigate(slides[currentIndex].link)}
+            style={{ cursor: 'pointer' }}
+          />
+          <div className="carousel-caption">
+            <h3>{slides[currentIndex].title}</h3>
+          </div>
+        </div>
+        <button className="carousel-button left" onClick={handlePrev}>
+          &#10094;
+        </button>
+        <button className="carousel-button right" onClick={handleNext}>
+          &#10095;
+        </button>
+      </div>
     </div>
   );
 };
