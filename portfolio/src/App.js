d4f45aa26e61815f8c4abf07c801430e05341bcb
Nilanjan Roy <roy.nil@northeastern.edu>
2025-04-09 18:13:44 -0400

    Prepare for GitHub Pages deployment

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Home from './Home';
