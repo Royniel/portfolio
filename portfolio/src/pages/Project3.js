f33da5208b4371792f4e04bff7bbd0ed62979c89
Nilanjan Roy <roy.nil@northeastern.edu>
2025-05-21 19:37:33 -0400

    Updated Git Ation Workflow

--- portfolio/src/pages/Project3.js
+++ portfolio/src/pages/Project3.js
@@ -2,7 +2,7 @@ const Project1 = () => {
     return (
       <div>
         <h1>Project 3: StockOptimate</h1>
-        <p> This project was React and Node.js with MSSQL as a DBMS. The purpose of this project was to create a standard Stock Management System.</p>
+        <p> This project was React and Node.js with MSSQL as a DBMS. The purpose of this project was to create a standard Stock Management System. The data was also Visualized with the help of Power Bi </p>
       </div>
     );
   };
